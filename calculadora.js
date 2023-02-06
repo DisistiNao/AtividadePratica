97739d99713c0045301755008c855c34da980b56
Gabriel Saldanha <gabrielsaldanha373@gmail.com>
2023-02-06 20:17:28 -0300

    change the structure of test.txt

--- calculadora.js
+++ calculadora.js
@@ -1,4 +1,7 @@
-// Para execultar o codigo, basta digitar node calculadora.js var1 var2
+const soma = () => {
+    console.log(parseInt(args[0]) + parseInt(args[1]));
+};
 
 const args = process.argv.slice(2);
-console.log(parseInt(args[0]) + parseInt(args[1]));
+
+soma();
