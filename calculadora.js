4e162f928c47f93dfcaae77bcc0300831ee17311
Gabriel Saldanha <gabrielsaldanha373@gmail.com>
2023-02-06 20:51:48 -0300

    refactor: calculator abstraction

--- calculadora.js
+++ calculadora.js
@@ -1,22 +1,9 @@
-const soma = () => {
-    console.log(parseInt(args[0]) + parseInt(args[1]));
-};
+var x = args[0];
+var y = args[2];
+var operator = args[1];
 
-const sub = () => {
-    console.log(parseInt(args[0]) - parseInt(args[1]));  
+function evaluate(param1, param2, operator) {
+  return eval(param1 + operator + param2);
 }
 
-const args = process.argv.slice(2);
-
-switch (args[0]) {
-    case 'soma':
-        soma();
-    break;
-
-    case 'sub':
-        sub();
-    break;
-
-    default:
-        console.log('does not support', arg[0]);
-}
+if ( console.log( evaluate(x, y, operator) ) ) {}
